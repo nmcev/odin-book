e074cc9b2c888088d54a0f8c1739d448389390ac
nmcev <1student.muhammed1@gmail.com>
2024-07-17 18:50:14 +0300

    style: fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,8 +11,8 @@ import clsx from 'clsx'
 import { NavItemProps } from '../types'
 
 export const Header: React.FC = () => {
-    return (
-      <header className='flex justify-between mx-36 my-2'>
+  return (
+      <header className='flex w-full gap-72 justify-center py-2 fixed backdrop:blur-xl top-0 bg-[rgba(255,255,255,0.85)] dark:backdrop:opacity-50 dark:bg-[#10101094]'>
         
           {/* logo section */}
         <div className=' rounded-lg px-8 py-4 grid items-center'>
