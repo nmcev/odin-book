95c481475aaa3b94e77da77d67b96a61d17c1c18
nmcev <1student.muhammed1@gmail.com>
2024-07-17 23:01:58 +0300

    feat: post interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,22 @@ export interface NavItemProps {
     children: React.ReactNode,
     to: string,
 }
+
+export interface PostInterface{
+    _id: number,
+    content: string,
+    media: string,
+    author: author,
+    likes: number,
+}
+
+export interface author {
+    username: string,
+    profilePic: string
+}
+
+
+export interface PostProps {
+    post: PostInterface
+}
+  
