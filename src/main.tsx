e34a951fe5c0263e975114f1c7055eec6949b07b
nmcev <1student.muhammed1@gmail.com>
2024-07-18 19:20:06 +0300

    feat: basic routes setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HomePage } from './pages/HomePage.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      }, {
+        path: '/search',
+        element: <h1>search</h1>
+      }, {
+        path: '/create-post',
+        element: <h1>search</h1>
+      }
+      , {
+        path: '/notifications',
+        element: <h1>search</h1>
+      },
+      {
+        path: '/profile',
+        element: <h1>search</h1>
+      }
+    ]
+  }, {
+    path: '/login',
+    element: <h1>Log in page</h1>
+  }
+  , {
+    path: '/signup',
+    element: <h1>Signup page</h1>
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
