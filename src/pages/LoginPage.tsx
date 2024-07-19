b51a190b4f59b9ca4fbfa3fbed7e8f1e02cc7d1d
nmcev <1student.muhammed1@gmail.com>
2024-07-20 00:23:53 +0300

    feat: setup login page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,95 @@
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+export const LoginPage: React.FC = () => {
+    const [pw, setPw] = useState('');
+    const [username, setUsername] = useState('');
+    const authContext = useContext(AuthContext)
+
+
+ 
+ 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await authContext?.login(username, pw)
+    };
+
+
+    if (authContext?.isLoggedIn) {
+        return (
+            <Navigate to={'/'}/>
+        )
+    }
+
+    return (
+        
+        <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            {!authContext?.isLoggedIn &&
+                <>
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">
+                    Sign in to your account
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <div>
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                            Username
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="username"
+                                name="username"
+                                type="text"
+                                required
+                                autoComplete="username"
+                                onChange={(e) => setUsername(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                                Password
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                autoComplete="current-password"
+                                onChange={(e) => setPw(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Not a member?{' '}
+                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                        Signup
+                    </a>
+                </p>
+            </div>
+                </>
+}
+        </div>
+    );
+};
