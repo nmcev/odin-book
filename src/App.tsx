c892794876cc77cfa213ea439e46d8f1314a50a8
nmcev <1student.muhammed1@gmail.com>
2024-07-19 13:58:48 +0300

    feat: wrap the App with post provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { Outlet } from "react-router-dom"
+import { Header } from "./components/Header"
+import { PostProvider } from "./contexts/PostContext"
 
 function App() {
 
   return (
     <>
-      <div className="">
-        <h1>
-            Threads book
-        </h1>
+
+      <PostProvider>
+      <div className="min-h-dvh relative">
+        <Header />
+        <Outlet />
       </div>
+      </PostProvider>
+
     </>
   )
 }
