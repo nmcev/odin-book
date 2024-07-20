466ee97cbf01ff9069e4e2b42b181c4ad1b2c80a
nmcev <1student.muhammed1@gmail.com>
2024-07-20 15:18:36 +0300

    feat: prevent sections while use is not logged in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState }  from 'react'
+import React, { useContext, useState, useEffect }  from 'react'
 import lightLogo from '/threads-logo-black.svg'
 import darkLogo from '/threads-logo-white.svg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { HomeIcon } from '../assets/icons/Navbar/HomeIcon'
 import { SearchIcon } from '../assets/icons/Navbar/SearchIcon'
 import { PostIcon } from '../assets/icons/Navbar/PostIcon'
@@ -95,20 +95,78 @@ export const Header: React.FC = () => {
 
 const NavItem: React.FC<NavItemProps> = ({ children, to }) => {
     const [isBouncing, setIsBouncing] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleClick = () => {
+    const handleClick = () => 
+    {
+      
+      if (!authContext?.isLoggedIn && to !== '/' && to !== '/search') {
+        setShowDialog(true);
+          return;
+      }
+      
       setIsBouncing(true);
-      setTimeout(() => setIsBouncing(false), 200);
+      setTimeout(() => {
+        setIsBouncing(false)
+        navigate(to)
+      }, 200);
     };
-
+  
+  const closeDialog = () => {
+    setShowDialog(false)
+  }
     
-    return (
-        <Link to={to}
+  return (
+      <>
+        <div 
             className={clsx('hover:bg-gray-100 rounded-lg px-8 py-5 grid items-center cursor-pointer', {
             'bounce': isBouncing,
       })}
+      
       onClick={handleClick}>
         {children}
-     </Link>
+      </div>
+      {showDialog && (
+        <Dialog message="You need to log in to access this page." onClose={closeDialog} />
+      )}
+      </>
     )
+}
+
+
+const Dialog: React.FC<{ message: string, onClose: () => void }> = ({ message, onClose }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    const handleEscapeClose = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', handleEscapeClose);
+
+    return () => {
+      document.body.style.overflow = 'auto';
+      document.removeEventListener('keydown', handleEscapeClose);
+    };
+  }, [onClose]);
+
+
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-zinc-900 bg-opacity-50'>
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <p>{message}</p>
+        <button
+          onClick={onClose}
+          className="mt-4 bg-zinc-500 text-white py-2 px-4 rounded hover:bg-zinc-600 transition-colors"
+          onKeyDown={close}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  )
+  
 }
