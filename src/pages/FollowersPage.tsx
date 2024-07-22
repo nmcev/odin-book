bb1551b7673350505355be61d9d8560fb566a855
nmcev <1student.muhammed1@gmail.com>
2024-07-22 21:28:18 +0300

    refactor: pass followers to users list component

--- src/pages/FollowersPage.tsx
+++ src/pages/FollowersPage.tsx
@@ -29,7 +29,7 @@ export const FollowersPage:  React.FC  = () => {
     
     return (
       <div className="min-h-screen mt-24 flex flex-col items-center divide-y divide-gray-300 gap-4 px-4">
-        { followers && user && <UsersList followers={followers} userId={user?._id} />}
+        { followers && user && <UsersList followers={user.followers}  />}
     </div>
 
   )
