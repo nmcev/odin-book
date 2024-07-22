783fabac1d1e0c4849a0ef60e2824ffef6282323
nmcev <1student.muhammed1@gmail.com>
2024-07-22 14:35:02 +0300

    feat: users list to render followers and following

---
+++ src/components/UsersList.tsx
@@ -0,0 +1,91 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext';
+import { FollowContext } from '../contexts/FollowContext';
+import { User } from '../types';
+
+interface UsersListProps {
+  followers: User[];
+  userId: string;
+}
+export const UsersList: React.FC<UsersListProps> = ({ followers, userId }) => {
+    
+    const followContext = useContext(FollowContext);
+    
+  const setFollowing = followContext?.setFollowing;
+  const following = followContext?.following
+    const authContext = useContext(AuthContext);
+    const currentUser = authContext?.user
+
+    async function unfollow(userId: string) {
+        const res = await fetch(`http://localhost:3000/api/unfollow/${userId}`, {
+          credentials: 'include',
+          method: "DELETE",
+        })
+        
+        if (res.ok && setFollowing && following) {
+          setFollowing(following.filter(user => user._id !== userId));
+        }
+      }
+    
+    
+      const follow = async (userId: string) => {
+        const res = await fetch(`http://localhost:3000/api/follow/${userId}`, {
+            credentials: 'include',
+            method: 'POST',
+        });
+  
+        if (res.ok && setFollowing) {
+          const followedUser = followers.find(user => user._id === userId);
+    if (followedUser && following) {
+      setFollowing([...following, followedUser]);
+    }
+          
+        }
+      };
+      const isFollowing = (followerId: string) => {
+        return following && following.some(followedUser => followedUser._id === followerId);
+      }
+    return (
+        <>
+      {followers?.map((follower) => (
+        <div key={follower._id} className="flex items-center justify-between w-full max-w-xl p-4 ">
+          <div className="flex items-center gap-4 flex-1">
+            <img
+              src={follower.profilePic}
+              alt={follower.username}
+              className="w-16 h-16 rounded-full object-cover border border-gray-300"
+            />
+            <h2 className="text-lg font-semibold text-gray-800">{follower.username}</h2>
+          </div>
+    
+            { currentUser?._id === userId && (
+                    <button
+
+                 onClick={() => {
+                if (isFollowing(follower._id) ) {
+                      unfollow(follower._id)
+                } else {
+                  follow(follower._id)
+                    }
+                  }}
+                        className={`px-4 py-2 rounded-lg text-white transition-colors ${
+                          isFollowing(follower._id)  ? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-600'
+                        }`}
+                      >
+                            {isFollowing(follower._id) ? 'Unfollow' : 'Follow'}
+                            </button>
+                      
+                )
+          }    
+
+        </div>
+      ))}
+            
+        {
+          followers.length === 0 && (
+            <h1>No users</h1>
+          )
+        }
+      </>
+  )
+}
