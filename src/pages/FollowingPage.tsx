d0a9741693f575be0adc59cee3ea796a38bf0c4c
nmcev <1student.muhammed1@gmail.com>
2024-07-22 14:37:24 +0300

    feat: build following page

---
+++ src/pages/FollowingPage.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+import { UsersList } from '../components/UsersList';
+import { FollowContext } from '../contexts/FollowContext';
+
+export const FollowingPage = () => {
+    const authContext = useContext(AuthContext);
+    const user = authContext?.user;
+    const followContext = useContext(FollowContext)
+
+
+
+    console.log(user?.following)
+    return (
+        <div className="min-h-screen mt-24 flex flex-col items-center divide-y divide-gray-300 gap-4 px-4">
+            { followContext && user &&  <UsersList userId={user?._id} followers={followContext?.following} />}
+        </div>
+  )
+}
