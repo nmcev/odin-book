da8d64395f7319200604420d12f0334e5ff6a79d
nmcev <1student.muhammed1@gmail.com>
2024-08-03 23:54:52 +0300

    feat: handle pfp change

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -16,13 +16,17 @@ export const ProfilePage:React.FC = () => {
     const postContext = useContext(PostContext)
     const [reposts, setReposts] = useState<PostInterface[]>([])
     const [posts, setPosts] = useState<PostInterface[]>([])
-
+    const [pfp, setPfp] = useState('');
 
 
     useEffect(() => {
         if (user?.posts) setPosts(user?.posts )
     }, [user?.posts])
     
+    useEffect(() => {
+       user?.profilePic && setPfp(user?.profilePic)
+
+    }, [user?.profilePic])
     useEffect(() => {
        if (user?.repostedPosts) setReposts(user?.repostedPosts);
 
@@ -84,6 +88,44 @@ export const ProfilePage:React.FC = () => {
             
         }
     }
+
+
+    const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+
+        if (file) {
+
+            const formData = new FormData();
+
+            formData.append('file', file);
+            formData.append('upload_preset',import.meta.env.VITE_UPLOAD_PRESETS );
+
+
+                    const cloudinaryFetch = await fetch(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_PRESESTS_NAME}/image/upload`, {
+                        method: 'POST',
+                        body: formData,
+                    });      
+                    
+                    const result = await cloudinaryFetch.json();
+
+                    setPfp(result.secure_url)
+
+                    console.log(result)
+                    
+                    await fetch('http://localhost:3000/api/edit-profile', {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        credentials: 'include',
+                        body: JSON.stringify({profilePic: result.secure_url})
+                    })
+
+
+        }
+
+    }
+
     
     return (
         <div className="min-h-screen mt-24  mx-auto flex  flex-col  gap-8 max-w-2xl">
@@ -103,17 +145,18 @@ export const ProfilePage:React.FC = () => {
                      {/* image section */}
                     <div className='rounded-full'>
                         <div className='relative'>
-                        <img  src={user?.profilePic || ''} className='object-fill rounded-full max-w-[84px]' alt={user?.name} />
+                        <img  src={pfp} className='object-fill rounded-full max-w-[84px]' alt={user?.name} />
                             <div className='w-6 h-6 absolute right-0 bottom-0 bg-blue-400 flex justify-center items-center rounded-full'>
                             <input 
                                 type='file' 
                                 accept='image/*' 
                                 className='absolute right-0 bottom-0 opacity-0 cursor-pointer' 
                                 id='file-input'
+                                onChange={handleImageChange}
                                 />
                                 
                             <label htmlFor='file-input' className='w-6 h-6 absolute right-0 bottom-0 bg-blue-400 flex justify-center items-center rounded-full cursor-pointer'>
-                            <svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M20.8477 1.87868C19.6761 0.707109 17.7766 0.707105 16.605 1.87868L2.44744 16.0363C2.02864 16.4551 1.74317 16.9885 1.62702 17.5692L1.03995 20.5046C0.760062 21.904 1.9939 23.1379 3.39334 22.858L6.32868 22.2709C6.90945 22.1548 7.44285 21.8693 7.86165 21.4505L22.0192 7.29289C23.1908 6.12132 23.1908 4.22183 22.0192 3.05025L20.8477 1.87868ZM18.0192 3.29289C18.4098 2.90237 19.0429 2.90237 19.4335 3.29289L20.605 4.46447C20.9956 4.85499 20.9956 5.48815 20.605 5.87868L17.9334 8.55027L15.3477 5.96448L18.0192 3.29289ZM13.9334 7.3787L3.86165 17.4505C3.72205 17.5901 3.6269 17.7679 3.58818 17.9615L3.00111 20.8968L5.93645 20.3097C6.13004 20.271 6.30784 20.1759 6.44744 20.0363L16.5192 9.96448L13.9334 7.3787Z" fill="#0F0F0F"></path> </g></svg>
+                            <svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M20.8477 1.87868C19.6761 0.707109 17.7766 0.707105 16.605 1.87868L2.44744 16.0363C2.02864 16.4551 1.74317 16.9885 1.62702 17.5692L1.03995 20.5046C0.760062 21.904 1.9939 23.1379 3.39334 22.858L6.32868 22.2709C6.90945 22.1548 7.44285 21.8693 7.86165 21.4505L22.0192 7.29289C23.1908 6.12132 23.1908 4.22183 22.0192 3.05025L20.8477 1.87868ZM18.0192 3.29289C18.4098 2.90237 19.0429 2.90237 19.4335 3.29289L20.605 4.46447C20.9956 4.85499 20.9956 5.48815 20.605 5.87868L17.9334 8.55027L15.3477 5.96448L18.0192 3.29289ZM13.9334 7.3787L3.86165 17.4505C3.72205 17.5901 3.6269 17.7679 3.58818 17.9615L3.00111 20.8968L5.93645 20.3097C6.13004 20.271 6.30784 20.1759 6.44744 20.0363L16.5192 9.96448L13.9334 7.3787Z" fill="#0F0F0F"></path> </g></svg>
                             </label>
 
                             </div>
