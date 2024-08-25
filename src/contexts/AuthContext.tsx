5864b16aecf35bf34c09b4ed62060e0b2eb0aad6
nmcev <1student.muhammed1@gmail.com>
2024-08-25 18:59:57 +0300

    feat: build register function and credentials state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,10 +3,17 @@ import { FC, ReactNode, createContext, useState, useEffect } from "react";
 export const AuthContext = createContext<AuthContextProps | null>(null);
 import { User } from "../types";
 
+interface UserCredentials {
+    username: string;
+    password: string;
+}
+
 interface AuthContextProps {
     login: (username: string, password: string) => void;
+    register: (username: string, password: string, name: string, bio: string, file: File) => void;
     logout: () => void; 
-
+    setUserCredentials: (credentials: UserCredentials) => void;
+    credentials: UserCredentials
     user: User | null,
     isLoggedIn: boolean;
     loading: boolean;
@@ -17,6 +24,12 @@ interface AuthContextProviderProps {
 }
 
 export const AuthContextProvider: FC<AuthContextProviderProps> = ({ children }) => {
+    const [credentials, setCredentials] = useState<{ username: string, password: string }>({ username: '', password: '' });
+
+    const setUserCredentials = (creds: { username: string, password: string }) => {
+        setCredentials(creds);
+    };
+
     const [user, setUser] = useState<User | null>(null);
     const [isLoggedIn, setIsLoggedIn] = useState<boolean | false>(false);
     const [loading, setLoading] = useState(true);
@@ -65,6 +78,50 @@ export const AuthContextProvider: FC<AuthContextProviderProps> = ({ children }) 
 
 
 
+    const register = async (username: string, password: string, name: string, bio: string, file: File) => {
+        try {
+
+
+                const formData = new FormData();
+    
+                formData.append('file', file);
+                formData.append('upload_preset',import.meta.env.VITE_UPLOAD_PRESETS );
+    
+    
+                        const cloudinaryFetch = await fetch(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_PRESESTS_NAME}/image/upload`, {
+                            method: 'POST',
+                            body: formData,
+                        });      
+                        
+                        const result = await cloudinaryFetch.json();
+    
+    
+            const pfp = result.secure_url
+    
+    
+          
+
+
+            const res = await fetch('http://localhost:3000/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({ username, password, profilePic: pfp, bio, name }),
+            });
+
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+
+            await login(username, password)
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
+    };
+
     useEffect(() => {
         const checkSession = async () => {
             try {
@@ -93,7 +150,7 @@ export const AuthContextProvider: FC<AuthContextProviderProps> = ({ children }) 
     }, []);
 
     return ( 
-        <AuthContext.Provider value={{ login, isLoggedIn, user, loading , logout}}>
+        <AuthContext.Provider value={{ login, isLoggedIn, user, loading , logout, register, setUserCredentials, credentials}}>
             {loading ? <p>Loading...</p> : children} 
         </AuthContext.Provider>
     );
