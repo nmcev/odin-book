ed833070355e34bd306b1a3e336f2a2b6697bd0e
nmcev <1student.muhammed1@gmail.com>
2024-08-26 18:35:02 +0300

    style: fill the headers icons with white on dark mode

--- src/assets/icons/Navbar/PostIcon.tsx
+++ src/assets/icons/Navbar/PostIcon.tsx
@@ -9,9 +9,9 @@ export const PostIcon: React.FC<IconProps> = ({ h = 26, w = 26 }) => {
   const isActive = pathname === '/create-post'
 
   return (
-    <svg aria-label="Create" role="img" viewBox="0 0 26 26" className="x xffa9am x2lah0s x1jwls1v x1n2onr6 x17fnjtu x3egl4o" style={{ fill: "transparent", height: h, width: w }}><title>Create</title><path d="M22.75 13L22.75 13.15C22.75 16.5103 22.75 18.1905 22.096 19.4739C21.5208 20.6029 20.6029 21.5208 19.4739 22.096C18.1905 22.75 16.5103 22.75 13.15 22.75L12.85 22.75C9.48969 22.75 7.80953 22.75 6.52606 22.096C5.39708 21.5208 4.4792 20.6029 3.90396 19.4739C3.25 18.1905 3.25 16.5103 3.25 13.15L3.25 12.85C3.25 9.48968 3.25 7.80953 3.90396 6.52606C4.4792 5.39708 5.39708 4.4792 6.52606 3.90396C7.80953 3.25 9.48968 3.25 12.85 3.25L13 3.25"
+    <svg aria-label="Create" role="img" viewBox="0 0 26 26" className="x xffa9am x2lah0s x1jwls1v x1n2onr6 x17fnjtu x3egl4o" style={{ fill: "transparent", height: h, width: w }}><title>Create</title><path className={isActive ? "dark:stroke-white dark:fill-transparent" : "dark:stroke-white dark:fill-transparent"} d="M22.75 13L22.75 13.15C22.75 16.5103 22.75 18.1905 22.096 19.4739C21.5208 20.6029 20.6029 21.5208 19.4739 22.096C18.1905 22.75 16.5103 22.75 13.15 22.75L12.85 22.75C9.48969 22.75 7.80953 22.75 6.52606 22.096C5.39708 21.5208 4.4792 20.6029 3.90396 19.4739C3.25 18.1905 3.25 16.5103 3.25 13.15L3.25 12.85C3.25 9.48968 3.25 7.80953 3.90396 6.52606C4.4792 5.39708 5.39708 4.4792 6.52606 3.90396C7.80953 3.25 9.48968 3.25 12.85 3.25L13 3.25"
     stroke={isActive ? 'black' : 'rgb(144, 144, 144)'}
-      strokeLinecap="round" strokeWidth="2.5"></path><path d="M21.75 4.25L13.75 12.25"
+      strokeLinecap="round" strokeWidth="2.5"></path><path className={isActive ? "dark:stroke-white dark:fill-white" : "dark:stroke-white dark:fill-transparent"} d="M21.75 4.25L13.75 12.25"
       stroke={isActive ? 'black' : 'rgb(144, 144, 144)'}
       strokeLinecap="round" strokeWidth="2.5"></path></svg>
   )
--- src/assets/icons/Navbar/ProfileIcon.tsx
+++ src/assets/icons/Navbar/ProfileIcon.tsx
@@ -7,9 +7,9 @@ export const ProfileIcon:React.FC<IconProps> = ({h = 26, w = 26}) => {
   const pathname = location.pathname;
   const isActive = pathname === '/profile'
   return (
-    <svg aria-label="Profile" role="img" viewBox="0 0 26 26" className='x' style={{ fill: "transparent", height: h, width: w }}><title>Profile</title><circle cx="13" cy="7.25" r="4"
+    <svg aria-label="Profile" role="img" viewBox="0 0 26 26" className='x' style={{ fill: "transparent", height: h, width: w }}><title>Profile</title><circle className={isActive ? "dark:stroke-white dark:fill-white" : "dark:stroke-white dark:fill-transparent"} cx="13" cy="7.25" r="4"
     stroke={isActive ? 'black' : 'rgb(144, 144, 144)'}
-      strokeWidth="2.5"></circle><path d="M6.26678 23.75H19.744C21.603 23.75 22.5 23.2186 22.5 22.0673C22.5 19.3712 18.8038 15.75 13 15.75C7.19625 15.75 3.5 19.3712 3.5 22.0673C3.5 23.2186 4.39704 23.75 6.26678 23.75Z"
+      strokeWidth="2.5"></circle><path className={isActive ? "dark:stroke-white dark:fill-white" : "dark:stroke-white dark:fill-transparent"} d="M6.26678 23.75H19.744C21.603 23.75 22.5 23.2186 22.5 22.0673C22.5 19.3712 18.8038 15.75 13 15.75C7.19625 15.75 3.5 19.3712 3.5 22.0673C3.5 23.2186 4.39704 23.75 6.26678 23.75Z"
       stroke={isActive ? 'black' : 'rgb(144, 144, 144)'}
       strokeWidth="2.5"></path></svg>)
 }
