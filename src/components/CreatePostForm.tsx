c670f098a8e6a7ff27658355a8201e3a0113e6f1
nmcev <1student.muhammed1@gmail.com>
2024-08-26 19:13:43 +0300

    style(create post form): dark bg on textarea on dark mode

--- src/components/CreatePostForm.tsx
+++ src/components/CreatePostForm.tsx
@@ -73,10 +73,10 @@ export const CreatePostForm: React.FC = () => {
 
     return (
       <>
-<div className="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
+<div className="heading text-center font-bold text-2xl m-5 text-gray-800 dark:text-neutral-100">New Post</div>
 
       <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
-        <textarea  onChange={(e) =>  setContent(e.target.value)} value={content} className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellCheck="true" placeholder="What's on your mind right now🤔?"></textarea>
+        <textarea  onChange={(e) =>  setContent(e.target.value)} value={content} className="description bg-gray-100 dark:bg-[#101010] sec p-3 h-60 border border-gray-300 outline-none" spellCheck="true" placeholder="What's on your mind right now🤔?"></textarea>
 
           <div className="icons flex text-gray-500 m-2">
           <label>
@@ -112,7 +112,7 @@ export const CreatePostForm: React.FC = () => {
         )}
           
         <div className="buttons flex">
-          <div className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">Cancel</div>
+          <div className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto dark:text-neutral-100">Cancel</div>
           <div className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500" onClick={handleSubmitPost}>Post</div>
         </div>
          </div>
