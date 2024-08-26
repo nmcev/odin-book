0eb9889f4f9ca22b5efd2d9b2e41286ae0547499
nmcev <1student.muhammed1@gmail.com>
2024-08-26 18:12:46 +0300

    feat: show a message if there is no notification

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -8,6 +8,13 @@ const Notifications: React.FC = () => {
 
     const notifications = useContext(PostContext)?.notifications
 
+    if (notifications?.length === 0) {
+        return <>
+                <div className="min-h-screen flex items-center justify-center flex-col divide-y-[1.5px] ">
+                    <p className='text-lg text-gray-700 '>No Notifications</p>
+                </div>
+        </>
+    }
     return (
          <div className="min-h-screen my-24 flex items-center flex-col divide-y-[1.5px] ">
              {notifications?.map((notification) => (
