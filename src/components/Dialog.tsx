e1a12438e22b51e4b3594bb26aa6d8488ae7874b
nmcev <1student.muhammed1@gmail.com>
2024-08-28 20:26:49 +0300

    style: update dialog message style

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -20,8 +20,8 @@ export const Dialog: React.FC<{ message: string, onClose: () => void }> = ({ mes
   
     return (
       <div className='fixed inset-0 flex items-center justify-center bg-zinc-900 bg-opacity-50'>
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <p>{message}</p>
+        <div className="bg-white  dark:bg-neutral-600 p-6 rounded-lg shadow-lg">
+          <p className=' dark:text-neutral-100 px-2 font-bold '>{message}</p>
           <button
             onClick={onClose}
             className="mt-4 bg-zinc-500 text-white py-2 px-4 rounded hover:bg-zinc-600 transition-colors"
