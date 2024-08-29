323cd6262471d187c0d8b041b465cb7a24defc40
nmcev <1student.muhammed1@gmail.com>
2024-08-29 10:20:10 +0300

    style(header): improve responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export const Header: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-      <header className='sm:flex w-full sm:gap-72 justify-center py-2 fixed backdrop:blur-xl top-0 bg-[rgba(255,255,255,0.85)] dark:backdrop:opacity-50 dark:bg-[#10101094] z-50'>
+      <header className='sm:flex w-full justify-evenly sm:py-4 fixed backdrop:blur-xl top-0 bg-[rgba(255,255,255,0.85)] dark:backdrop:opacity-50 dark:bg-[#10101094] z-50'>
         
           {/* logo section */}
         <div className=' rounded-lg px-8 py-4 grid items-center'>
