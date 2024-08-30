700f405c93d5ba3a13adab695077153ff13d0cad
nmcev <1student.muhammed1@gmail.com>
2024-08-31 01:24:32 +0300

    style: remove bg color in user page

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -169,7 +169,7 @@ export const UserPage: React.FC = () => {
     <div className='flex flex-col gap-1 justify-center'>
 
         {/* info section */}
-        <div className='flex bg-white flex-1 gap-5 justify-between'>
+        <div className='flex flex-1 gap-5 justify-between'>
 
             <div className='flex-1'>
                 <h2 className='text-2xl font-bold'>{user?.name || 'name'}</h2>
