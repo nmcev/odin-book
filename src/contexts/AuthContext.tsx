1c106b5cc1d09b878a6cd88700a91412d4c9e64b
nmcev <1student.muhammed1@gmail.com>
2024-08-31 17:57:45 +0300

    feat: enhance loading ui

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -156,7 +156,21 @@ export const AuthContextProvider: FC<AuthContextProviderProps> = ({ children }) 
 
     return ( 
         <AuthContext.Provider value={{ login, isLoggedIn, user, loading , logout, register, setUserCredentials, credentials, loginError}}>
-            {loading ? <p>Loading...</p> : children} 
+          
+            {loading ? (
+                <div className="flex justify-center items-center h-screen">
+                <div
+            className="inline-block h-14 w-14 animate-spin  rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span>
+                    </div>
+                </div>
+            ) :
+                (
+                    children
+          )}
         </AuthContext.Provider>
     );
 };
