31b6fcffbb1a518c4ab86e832e2c38e2a9158593
nmcev <1student.muhammed1@gmail.com>
2024-09-01 00:55:38 +0300

    style: add bg color for the dropdown

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -79,7 +79,7 @@ export const LoginPage: React.FC = () => {
                                     <select
                                          onChange={handleUserSelect}
                                         id="user-select"
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-neutral-200 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     >
                                         <option value="">Select a user</option>
                                         {users.slice(0, 5).map((user) => (
