d9002f6c90aefeb1018f508bd84e93ec0e11303c
nmcev <1student.muhammed1@gmail.com>
2024-08-31 23:50:37 +0300

    feat: UI of editing profile info

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -17,6 +17,7 @@ export const ProfilePage:React.FC = () => {
     const [posts, setPosts] = useState<PostInterface[]>([])
     const [pfp, setPfp] = useState('');
     const { reposts, setReposts, likePost, removeLike } = useContext(PostContext) || {};
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
 
 
     useEffect(() => {
@@ -29,6 +30,21 @@ export const ProfilePage:React.FC = () => {
     }, [user?.profilePic])
 
 
+    useEffect(() => {
+        const handleEscapeKey = (e: KeyboardEvent) => {
+          if (e.key === 'Escape') {
+            setIsEditDialogOpen(false);
+          }
+        };
+    
+        if (isEditDialogOpen) {
+          document.addEventListener('keydown', handleEscapeKey);
+        }
+    
+        return () => {
+          document.removeEventListener('keydown', handleEscapeKey);
+        };
+      }, [isEditDialogOpen]);
 
     const handleFollowersPage = (username: string) => {
         navigate(`/${username}/followers`);
@@ -168,7 +184,80 @@ export const ProfilePage:React.FC = () => {
                      </div>
                 </div>
                 
-                    <div className='flex flex-col gap-2 '>
+
+                <div className="mt-3">
+                            <button
+                                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                                onClick={() => setIsEditDialogOpen(true)}
+                            >
+                                Edit Profile 
+                            </button>
+                </div>
+
+                    {
+                    isEditDialogOpen && (
+
+                    <div className='fixed inset-0 flex items-center justify-center bg-zinc-900 bg-opacity-50' >
+                     <dialog open className="bg-white p-8 rounded-md">
+                                
+                                <p className='text-center font-bold text-xl'>Edit Profile</p>
+                                    
+                                <button className='border-b-[1px] w-full flex justify-start mb-4'  onClick={() => {
+
+                                    setIsEditDialogOpen(false)
+                                }}>
+                                        &times;
+                                    </button>
+                <form  className="space-y-6">
+                    <div>
+                        <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                            Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="name"
+                                name="name"
+                                type="text"
+                                required
+                                // onChange={(e) => setName(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900  dark:text-neutral-100 px-2  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="bio" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                            Bio
+                        </label>
+                        <div className="mt-2">
+                            <textarea
+                                id="bio"
+                                name="bio"
+                                required
+                                // onChange={(e) => setBio(e.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900   dark:text-neutral-100 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Save Profile
+                        </button>
+                    </div>
+                </form>
+            </dialog>    
+        </div>
+                        )
+                    }
+                    <div className='flex flex-col gap-2 -z-20 '>
                     <p className='text-[15px]'>{user?.bio}</p>
                     {user?.username && (
                         <div className='flex gap-3'>
