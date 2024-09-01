9f90fc8ea491020230706366de60521f00a3c159
nmcev <1student.muhammed1@gmail.com>
2024-09-01 15:33:30 +0300

    fix: add the missing username in the route of  the post page

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -126,7 +126,7 @@ export const Post: React.FC<PostProps> = ({ post, page, onLike, edit }) => {
             }
             
           {  page !== Page.PostPage &&        
-            <Link className='flex gap-1 items-center' to={`/${post.author}/${post._id}`} >
+            <Link className='flex gap-1 items-center' to={`/${post.author.username}/${post._id}`} >
             <CommentIcon />
               <span className=' font-normal  text-[#424242] dark:text-neutral-100 text-xs text-start'>{post.comments.length}</span>
               </Link>
